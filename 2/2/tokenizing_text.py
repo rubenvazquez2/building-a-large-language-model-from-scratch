9ed19a3351e537fe78954caf81edb129d910b7b3
Ruben Vazquez Jr. <rubenvazquez.2@outlook.com>
2025-03-13 20:27:44 -0400

    Add SimpleTokenizerV1 class.

--- 2/2/tokenizing_text.py
+++ 2/2/tokenizing_text.py
@@ -5,6 +5,7 @@ Description: Module with logic from section 2.2 of Building a Large Language Mod
 
 from urllib import request
 import logging, re
+from typing import Dict, List
 
 logger = logging.getLogger("tokenizing_text")
 logger.setLevel(logging.DEBUG)
@@ -12,6 +13,40 @@ s_handler = logging.StreamHandler()
 s_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger.addHandler(s_handler)
 s_handler.setFormatter(s_formatter)
+
+
+class SimpleTokenizerV1:
+    """
+    SimpleTokenizerV1
+    Description: A class that handles encoding and decoding of tokens and token ids
+    """
+
+    def __init__(self, vocab: Dict[str, int]) -> None:
+        self.str_to_int: Dict[str, int] = vocab
+        self.int_to_str: Dict[int, str] = {i: s for s, i in vocab.items()}
+
+    def encode(self, text: str) -> List[int]:
+        """
+        encode
+        Description: Convert text into a list of token ids
+        """
+        # Split text up into tokens
+        preprocessed = re.split(r"([,.:;?_!\"()']|--|\s)", text)
+        preprocessed = [item.strip() for item in preprocessed if item.split()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids: List[int]) -> str:
+        """
+        decode
+        Description: Convert list of token ids into text
+        """
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        # Remove spaces before specified punctuations
+        text = re.sub(r"\s+([,.?!\"()'])", r"\1", text)
+        return text
+
 
 if __name__ == "__main__":
     """
@@ -31,6 +66,22 @@ if __name__ == "__main__":
     logger.info("Total number of characters: %d", len(raw_text))
     logger.info("%s", raw_text[:99])
 
-    preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+    # Map each unique token to an integer
+    preprocessed = re.split(r"([,.:;?_!\"()']|--|\s)", raw_text)
     preprocessed = [item for item in preprocessed if item.split()]
-    logger.info(len(preprocessed))
+    all_words = sorted(set(preprocessed))
+    vocab_size = len(all_words)
+    logger.info(vocab_size)
+
+    vocab = {token: integer for integer, token in enumerate(all_words)}
+    for i, item in enumerate(vocab.items()):
+        logger.info(item)
+        if i >= 50:
+            break
+
+    tokenizer = SimpleTokenizerV1(vocab)
+    text = """"It's the last he painted, you know,"
+        Mrs. Gisburn said with pardonable pride."""
+    ids = tokenizer.encode(text)
+    logger.info(ids)
+    logger.info(tokenizer.decode(ids))
